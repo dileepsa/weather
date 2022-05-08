fc940802bab43363b547111f0987cf02f502ef89
Dileep A <99489054+dileepsa@users.noreply.github.com>
2022-05-08 15:40:36 +0530

    Created parser to parse csv to json.
    parser works for any kind of csv.

---
+++ parseCsv.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const readFile = (fileName) => {
+  return fs.readFileSync(fileName, 'utf8');
+}
+
+const toNumber = (value) => {
+  return +value;
+}
+
+const format = function (obj) {
+  obj.date = obj.date.split('-').map(toNumber);
+  return obj;
+}
+
+const convertToJSON = (keys, values, formatter) => {
+  let obj = {};
+  keys.forEach((key, index) => { obj[key] = +values[index] || values[index] });
+  obj = formatter ? formatter(obj) : obj;
+
+  return obj;
+}
+
+const parseCsv = (csvFile, seperator) => {
+  const dataStr = readFile(csvFile).split('\n');
+  const header = dataStr[0].split(seperator);
+  const data = dataStr.slice(1);
+
+  return data.map((row) => {
+    return convertToJSON(header, row.split(seperator), format);
+  })
+}
+
+const main = () => {
+  console.log(parseCsv('./weather.csv', '|'));
+};
+
+main();

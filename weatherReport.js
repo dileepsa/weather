94fbf1bb8f85b9800a9ac26d67437300ca8bd2c5
Dileep A <99489054+dileepsa@users.noreply.github.com>
2022-05-14 15:35:05 +0530

    Added min-max weather information.
    Refactored code.

--- weatherReport.js
+++ weatherReport.js
@@ -20,10 +20,101 @@ const writeFile = (fileName, content) => {
   return fs.writeFileSync(fileName, content, 'utf-8');
 };
 
+const createTableData = (data) => {
+  return createElement({
+    element: 'td',
+    content: data
+  })
+};
+
+const createBody = (location) => {
+  const values = [location.min, location.max];
+  const valuesHtml = values.map(createTableData).join('');
+
+  const row = createElement({
+    element: 'tr',
+    content: valuesHtml
+  });
+
+  const body = createElement({
+    element: 'tbody',
+    content: row
+  });
+
+  return body;
+};
+
+const createTableHeader = (header) => {
+  return createElement({
+    element: 'th',
+    content: header
+  })
+};
+
+const createHead = (headers) => {
+  const headersHtml = headers.map(createTableHeader).join('');
+
+  const head = createElement({
+    element: 'thead',
+    content: headersHtml,
+  });
+
+  return head;
+};
+
+const createTable = (location) => {
+  const headers = ['Min', 'Max'];
+  const head = createHead(headers);
+  const body = createBody(location);
+
+  const table = createElement({
+    element: 'table',
+    content: head + body,
+    classes: ['min-max']
+  });
+
+  return table;
+};
+
+const createDiv = (content, classes) => {
+  return createElement({
+    element: 'div',
+    content: content,
+    classes: classes
+  })
+};
+
+const weatherHtml = (location) => {
+  return createDiv(location.weather, ['weather']);
+};
+
+const temperatureHtml = (location) => {
+  return createDiv(location.temperature + ' °C', ['temperature']);
+};
+
+const cityHtml = (location) => {
+  return createDiv(location.city, ['city']);
+};
+
+const generateHtml = (location) => {
+  const image = img(location.weather, ['weather-image']);
+  const city = cityHtml(location);
+  const temperature = temperatureHtml(location);
+  const weather = weatherHtml(location);
+  const minMax = createTable(location);
+
+  const weatherInfo = createElement({
+    element: 'div',
+    content: city + temperature + weather + minMax,
+    classes: ['weather-info']
+  });
+
+  return image + weatherInfo;
+};
+
 const weather = (location) => location.temperature < 20 ? 'cloudy' : 'sunny';
 
 const extractHeaders = (data) => data.split('\n')[0].split('|');
-
 
 const extractLocation = (weatherData, place) => {
   const regEx = eval('/.*' + place + '.*/g');
@@ -33,9 +124,9 @@ const extractLocation = (weatherData, place) => {
   return location;
 };
 
-const getLocationInfo = (fileName, place) => {
-  const weatherData = readFile(fileName);
+const getLocationInfo = (weatherData, place) => {
   const headers = extractHeaders(weatherData);
+
   let location = extractLocation(weatherData, place);
   location = convertToObj(headers, location);
 
@@ -45,49 +136,12 @@ const getLocationInfo = (fileName, place) => {
   return location;
 };
 
-const cityHtml = (location) => {
-  return createElement({
-    element: 'h3',
-    content: location.city,
-    classes: ['city'],
-  });
-};
-
-const temperatureHtml = (location) => {
-  return createElement({
-    element: 'div',
-    content: location.temperature + ' C',
-    classes: ['temperature']
-  });
-};
-
-const weatherHtml = (location) => {
-  return createElement({
-    element: 'div',
-    content: location.weather,
-    classes: ['weather']
-  });
-};
-
-const generateHtml = (location) => {
-  const image = img(location.weather, ['weather-image']);
-  const city = cityHtml(location);
-  const temperature = temperatureHtml(location);
-  const weather = weatherHtml(location);
-
-  const weatherInfo = createElement({
-    element: 'div',
-    content: city + temperature + weather,
-    classes: ['weather-info']
-  });
-
-  return image + weatherInfo;
-};
-
 const main = (dataFile, template) => {
   const place = process.argv[2];
-  const location = getLocationInfo(dataFile, place);
+  const weatherData = readFile(dataFile);
+  const location = getLocationInfo(weatherData, place);
   const weatherHtml = generateHtml(location);
+
   let html = readFile(template);
 
   html = html.replace(/__CONTENT__/, weatherHtml);
